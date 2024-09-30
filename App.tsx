c76b0e755d41b79f08b5702296db974c61ea7d63
Mostafa.shafie <mostafa.shafie@hotmail.com>
2024-09-30 19:53:44 +0400

    Updated

--- App.tsx
+++ App.tsx
@@ -1,26 +1,12 @@
-// App.tsx
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import ActiveRepLandingPage from "./pages/ActiveRepLandingPage";
-import CoachingReportPage from "./pages/CoachingReportPage";
-import DashboardsPage from "./pages/DashboardsPage";
-import YourJourney from "./components/YourJourney";
-import Planner from "./components/Planner";
-import VisitDetails from "./components/VisitDetails";
+import React from 'react';
+import ActiveRepLandingPage from './ActiveRepLandingPage';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<ActiveRepLandingPage />} />
-        <Route path="/coaching-report" element={<CoachingReportPage />} />
-        <Route path="/dashboards" element={<DashboardsPage />} />
-        <Route path="/your-journey" element={<YourJourney />} />
-        <Route path="/planner" element={<Planner />} />
-        <Route path="/visit-details" element={<VisitDetails />} />
-      </Routes>
-    </Router>
+    <div>
+      <ActiveRepLandingPage />
+    </div>
   );
-}
+};
 
 export default App;
